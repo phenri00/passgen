bac9a52280e795750d5e1ea34a14afb5b0205fb4
henriksson <patrick.henriksson@drivec.se>
2019-12-26 09:33:17 +0100

    modified:   main.go

--- main.go
+++ main.go
@@ -7,24 +7,22 @@ import (
 	"math/big"
 )
 
-//const length = 12
-
 func main() {
 
 	length := flag.Int("num", 8, "password length")
 
 	flag.Parse()
 
-	//Aa-Zz
+	//aA-zZ
 	charMap := append(charSlice(65, 90), charSlice(97, 122)...)
 	//0-9
 	charMap = append(charMap, charSlice(48, 57)...)
 	//special char
 	charMap = append(charMap, charSlice(33, 47)...)
 	//special char
-	charMap = append(charMap, charSlice(91, 96)...)
-	//special char
 	charMap = append(charMap, charSlice(58, 64)...)
+	//special char
+	charMap = append(charMap, charSlice(91, 96)...)
 
 	for i := 1; i <= *length; i++ {
 		fmt.Printf("%c", charMap[randNum(len(charMap))])
